4e3b883f2413f88942ade00b2972afe51addad6d
tvlangley <chibia2@gmail.com>
2017-08-24 17:18:41 -0400

    Update 020/master from browser-template
    Refresh for cohort 020

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,11 +11,4 @@ $(() => {
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-require('./example')
-
-const authEvents = require('./auth/events.js')
-
-// On document ready
-$(() => {
-  authEvents.addHandlers()
-})
+// require('./example')
